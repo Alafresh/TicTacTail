d029ddef421b59194d2f7cd6ac7fedf471eff799
Alafresh <51685896+Alafresh@users.noreply.github.com>
2025-03-22 16:59:02 -0500

    [add] gridposition, cross and circle prefab

--- Assets/Scripts/GridPosition.cs
+++ Assets/Scripts/GridPosition.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class GridPosition : MonoBehaviour
 {
-   
+    [SerializeField] private int x;
+    [SerializeField] private int y;
+    private void OnMouseDown()
+    {
+        Debug.Log("Click it" + x + y);
+        GameManager.Instance.ClickedOnGridPosition(x, y);
+    }
 }
